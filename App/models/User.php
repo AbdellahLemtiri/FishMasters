350181de550499e5b4c282c397c56267a063c063
haddadaziz <aziz.haddad003@gmail.com>
2026-01-21 15:29:46 +0100

    add : login and register

--- App/models/User.php
+++ App/models/User.php
@@ -52,5 +52,51 @@ class User {
     public function setIdRole($idRole) {
         $this->idRole = $idRole;
     }
+
+    public function register($nom, $email, $password, $idRole) {
+        $query = 'INSERT INTO ' . $this->table . ' 
+                  ("nomUser", "emailUser", "passwordUser", "idRole") 
+                  VALUES (:nom, :email, :pass, :role)';
+        
+        $stmt = $this->conn->prepare($query);
+
+        $nom = htmlspecialchars(strip_tags($nom));
+        $email = htmlspecialchars(strip_tags($email));
+        
+
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':pass', $passwordHash);
+        $stmt->bindParam(':role', $idRole);
+
+        if($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function login($email, $password) {
+        $query = 'SELECT * FROM ' . $this->table . ' WHERE "emailUser" = :email';
+        
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+
+        if($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            
+            if(password_verify($password, $row['passwordUser'])) {
+                $this->setIdUser($row['idUser']);
+                $this->setNomUser($row['nomUser']);
+                $this->setEmailUser($row['emailUser']);
+                $this->setIdRole($row['idRole']);
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
 }
 ?>

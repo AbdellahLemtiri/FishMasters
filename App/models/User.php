b52bee002c3b495540e282c8c76627bceded4661
haddadaziz <aziz.haddad003@gmail.com>
2026-01-21 15:22:54 +0100

    add : getters and setters for user and role table

--- App/models/User.php
+++ App/models/User.php
@@ -5,15 +5,52 @@ class User {
     private $conn;
     private $table = "users";
 
-    public $idUser;
-    public $nomUser;
-    public $emailUser;
-    public $passwordUser;
-    public $roleUser;
-    public $idRole;
+    private $idUser;
+    private $nomUser;
+    private $emailUser;
+    private $passwordUser;
+    private $roleUser;
+    private $idRole;
     public function __construct() {
         $database = new Database();
         $this->conn = $database->getConnection();
+    }
+    public function getIdUser(){
+        return $this->idUser;
+    }
+    public function getNomUser(){
+        return $this->nomUser;
+    }
+    public function getEmailUser(){
+        return $this->emailUser;
+    }
+    public function getPasswordUser(){
+        return $this->passwordUser;
+    }
+    public function getRoleUser(){
+        return $this->roleUser;
+    }
+    public function getIdRole(){
+        return $this->idRole;
+    }
+    public function setIdUser($idUser) {
+        $this->idUser = $idUser;
+    }
+
+    public function setNomUser($nomUser) {
+        $this->nomUser = $nomUser;
+    }
+
+    public function setEmailUser($emailUser) {
+        $this->emailUser = $emailUser;
+    }
+
+    public function setPasswordUser($passwordUser) {
+        $this->passwordUser = $passwordUser;
+    }
+
+    public function setIdRole($idRole) {
+        $this->idRole = $idRole;
     }
 }
 ?>

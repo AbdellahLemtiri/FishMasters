2fc26b8b990891cc077c90370bbc462f85f7fb9f
kuroi-hime <kuroihime095@gmail.com>
2026-01-25 20:06:11 +0100

    model: adapter le modèle fan aux nouveaux changements.

--- App/models/fan.php
+++ App/models/fan.php
@@ -22,34 +22,37 @@
             return $this->dateInscription;
         }
 
-        static function getByEmail(string $email):Fan{
+        public function __toString():string{
+            return "Fan (statut:{$this->statut_fan}): {$this->nomuser} inscrit le : {$this->dateInscription} avec l'email suivant: {$this->emailuser}.";
+        }
+
+        static function getByEmail(string $email):?Fan{
             try {
                 $sql = "SELECT * 
                         FROM fans
-                        WHERE emailUser = :emailUser";
+                        WHERE emailuser = :emailuser";
                 $pdo = Connexion::connect()->getConnexion();
                 $stmt = $pdo->prepare($sql);
-                $stmt->execute(['emailUser' => $fan->emailUser]);
+                $stmt->execute(['emailuser' => $email]);
                 $stmt->setFetchMode(PDO::FETCH_CLASS, self::class);
-
-                return $stmt->fetch();
+                $fan = $stmt->fetch();
             } catch (PDOException $e) {
                 error_log('Erreur lors de l\'insertion de fan:\n' . $e->getMessage() . '\n------------------\n');
-                return null;
             }
+            return $fan ? $fan:null;
         }
 
         static function ajouterFan(Fan $fan):bool{
             try {
-                $sql = "INSERT INTO fans (nomUser, emailUser, passwordUser, role_id, statut_fan)
+                $sql = "INSERT INTO fans (nomuser, emailuser, passworduser, roleid, statut_fan)
                         VALUES (:nomUser, :emailUser, :passwordUser, :role_id, :statut_fan)";
                 $pdo = Connexion::connect()->getConnexion();
                 $stmt = $pdo->prepare($sql);
                 $stmt->execute([
-                    'nomUser' => $fan->nomUser,
-                    'emailUser' => $fan->emailUser,
-                    'passwordUser' => password_hash($fan->passwordUser, PASSWORD_DEFAULT),
-                    'role_id' => $fan->role_id,
+                    'nomUser' => $fan->nomuser,
+                    'emailUser' => $fan->emailuser,
+                    'passwordUser' => $fan->passworduser,
+                    'role_id' => $fan->roleid,
                     'statut_fan' => $fan->statut_fan
                 ]);
 
@@ -60,20 +63,20 @@
             }
         }
 
-        public function modifierFan(Fan $fan):bool{
+        static function modifierFan(Fan $fan):bool{
             try {
                 $sql = "UPDATE fans
-                        SET nomUser = :nomUser, emailUser = :emailUser, passwordUser = :passwordUser, role_id = :role_id, statut_fan = :statut_fan
-                        WHERE idUser = :idUser";
+                        SET nomuser = :nomUser, emailuser = :emailUser, passworduser = :passwordUser, roleid = :role_id, statut_fan = :statut_fan
+                        WHERE iduser = :idUser";
                 $pdo = Connexion::connect()->getConnexion();
                 $stmt = $pdo->prepare($sql);
                 $stmt->execute([
-                    'nomUser' => $fan->nomUser,
-                    'emailUser' => $fan->emailUser,
-                    'passwordUser' => password_hash($fan->passwordUser, PASSWORD_DEFAULT),
-                    'role_id' => $fan->role_id,
+                    'nomUser' => $fan->nomuser,
+                    'emailUser' => $fan->emailuser,
+                    'passwordUser' => $fan->passworduser,
+                    'role_id' => $fan->roleid,
                     'statut_fan' => $fan->statut_fan,
-                    'idUser' => $this->idUser
+                    'idUser' => $fan->iduser
                 ]);
 
                 return true;
@@ -86,10 +89,10 @@
         public function supprimerFan():bool{
             try {
                 $sql = "DELETE FROM fans
-                        WHERE idUser = :idUser";
+                        WHERE iduser = :idUser";
                 $pdo = Connexion::connect()->getConnexion();
                 $stmt = $pdo->prepare($sql);
-                $stmt->execute(['idUser' => $this->idUser]);
+                $stmt->execute(['idUser' => $this->iduser]);
 
                 return true;
             } catch (PDOException $e) {
@@ -98,18 +101,18 @@
             }
         }
 
-        static function getById(int $id_fan):Fan{
+        static function getById(int $id_fan):?Fan{
             try {
-                $sql = "SELECT * FROM fans WHERE idUser = :idUser";
+                $sql = "SELECT * FROM fans WHERE iduser = :idUser";
                 $pdo = Connexion::connect()->getConnexion();
                 $stmt = $pdo->prepare($sql);
                 $stmt->execute(['idUser' => $id_fan]);
                 $stmt->setFetchMode(PDO::FETCH_CLASS, self::class);
-
-                return $stmt->fetch();
+                $fan = $stmt->fetch();
             } catch (PDOException $e) {
                 error_log('Erreur lors de la récupération du fan:\n' . $e->getMessage() . '\n------------------\n');
-                return null;
+            } finally{
+                return $fan ? $fan:null;
             }
         }
 
@@ -119,11 +122,11 @@
                 $pdo = Connexion::connect()->getConnexion();
                 $stmt = $pdo->query($sql);
                 $stmt->execute();
-
-                return $stmt->fetchAll(PDO::FETCH_CLASS, self::class);
+                $fans = $stmt->fetchAll(PDO::FETCH_CLASS, self::class);
             } catch (PDOException $e) {
                 error_log('Erreur lors de la récupération des fans:\n' . $e->getMessage() . '\n------------------\n');
-                return [];
+            } finally{
+                return $fans ? $fans:[];
             }
         }
     }
